acb124704a9f05f63ae6a17dfd11b3563d5e5174
martinluthertiani <martinluther.tiani@gmail.com>
2020-04-03 01:27:58 +0200

    Coucou! Très content de mon travail

--- FILMS_GENRES/insert_one_row_films_genres.py
+++ FILMS_GENRES/insert_one_row_films_genres.py
@@ -8,13 +8,13 @@ try:
     # OM 2020.01.28 Une instance "insert_records" pour permettre l'utilisation des méthodes de la classe DbInsertOneTable
     insert_records = insert_one_table.DbInsertOneTable()
 
-    valeur_ins_fkgenres = 10
-    valeur_ins_fkfilm = 3
+    valeur_ins_fkPers = 1
+    valeur_ins_fkTeleph = 3
     # Afficher les valeurs dans la console...c'est tout, vraiment tout !
-    print("valeur_fkgenre ",valeur_ins_fkgenre, "valeur_fkfilm ",valeur_ins_fkfilm)
+    print("valeur_fkPers ",valeur_ins_fkPers, "valeur_fkTeleph ",valeur_ins_fkTeleph)
 
     # Définitions d'un dictionnaire pour passer les valeurs en paramètres de façon un "peu" sécurisée dans la BD
-    valeurs_insertion_dictionnaire = {'value_fkgenre': valeur_ins_fkgenre, 'value_fkfilm': valeur_ins_fkfilm}
+    valeurs_insertion_dictionnaire = {'value_fkPers': valeur_ins_fkPers, 'value_fkTeleph': valeur_ins_fkTeleph}
 
     # OM 2020.01.28 Pour éviter les injections SQL, il est possible de passer les valeurs à insérer sous forme "paramètrée" (avec le %(...)s au lieu de %s)
     # Pour les vrais geeks et geeketes consulter le site ci-dessous.
@@ -23,13 +23,13 @@ try:
     # Une longue chaîne de caractères (format PEP8 selon proposition de PyCharm)
     # Je décide d'insèrer 2 on voit ainsi la correspondance des positions entre les attributs
     # de la BD et les variables Python définies juste en dessus.
-    mysql_insert_string = "INSERT INTO t_genres_films (id_genre_film, fk_genre, fk_film, date_insert_genre) VALUES " \
-                          "(NULL, %(value_fkgenre)s, %(value_fkfilm)s, CURRENT_TIMESTAMP) "
+    mysql_insert_string = "INSERT INTO t_Pers_Teleph (id_Pers_Teleph, fk_Pers, fk_Teleph, DateEnreg_Teleph) VALUES " \
+                          "(NULL, %(value_fkPers)s, %(value_fkTeleph)s, CURRENT_TIMESTAMP) "
     # Insertion des valeurs définie dans la variable dictionnaire "valeurs_insertion_dictionnaire"
-    # dans la table "t_genres_films"
+    # dans la table "t_Pers_Teleph"
     insert_records.insert_one_record_many_values_one_table(mysql_insert_string,
                                                            valeurs_insertion_dictionnaire)
 
 except Exception as erreur:
-    # OM 2020.03.01 Message en cas d'échec du bon déroulement des commandes ci-dessus.
+    # OM 2020.04.03 Message en cas d'échec du bon déroulement des commandes ci-dessus.
     print("error message: {0}".format(erreur))
