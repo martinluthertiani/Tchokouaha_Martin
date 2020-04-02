880f31f83e5f6ea271a7b04580cd2e2dc64cfc24
martinluthertiani <martinluther.tiani@gmail.com>
2020-04-03 00:09:28 +0200

    Bonjour Mister OM; ça marche!!!!!!!

--- FILMS_GENRES/select_all_films_genres.py
+++ FILMS_GENRES/select_all_films_genres.py
@@ -5,15 +5,15 @@
 import json
 
 from DATABASE.SELECT import select_table
-
+#Bonjour Monsieur OM; ça marche!!
 try:
     # OM 2020.03.26 Une instance "select_record" pour permettre l'utilisation des méthodes de la classe DbSelectOneTable
     select_record = select_table.DbSelectOneTable()
     # Pour l'affichage du contenu suite à une requête SELECT avec un tri sur la colonne id_genre
-    # Cette requête provient du fichier à disposition dans mon gitlab "REQUETES_NOM_PRENOM_SUJET_BD_104.sql"
-    mysql_select_string = ("SELECT id_genre, intitule_genre , id_film, nom_film FROM t_genres_films AS T1\n"
-                           "INNER JOIN t_films AS T2 ON T2.id_film = T1.fk_film\n"
-                           "INNER JOIN t_genres AS T3 ON T3.id_genre = T1.fk_genre")
+    # Cette requête provient du fichier à disposition dans mon gitlab "REQUETES_tiani_martin_gerbersytemsclients_1c_2020.sql"
+    mysql_select_string = ("SELECT id_Pers, Nom_Pers , id_Mails, Nom_Mail FROM t_Pers_Mails AS T1\n"
+                           "INNER JOIN t_Mails AS T2 ON T2.id_Mails = T1.fk_Mail\n"
+                           "INNER JOIN t_Personnes AS T3 ON T3.id_Pers = T1.fk_Pers")
     # Les résultats de la requête se trouvent dans la variable "records_select" de type <class 'list'>
     records_select = select_record.select_rows(mysql_select_string)
 
@@ -21,10 +21,10 @@ try:
     print("Type de type records_select : ",type(records_select),"Tous les résultats ", records_select, "Type des résultats ")
 
     for row in records_select:
-        print(row['nom_film'],row['intitule_genre'],)
+        print(row['Nom_Mail'],row['Nom_Pers'],)
 
     for row in records_select:
-        output = "nom du film: {nom_film}  genre: {intitule_genre}"
+        output = "nom du Mail: {Nom_Mail}  Personne: {Nom_Pers}"
         print(output.format(**row))
 
     # Le meilleur pour la fin : le module pymysql intègre la conversion en JSON  avec "cursorclass=pymysql.cursors.DictCursor"
