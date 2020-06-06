39700b73a50f4f68da54c4017ebe8cce41dabe1b
martinluthertiani <martinluther.tiani@gmail.com>
2020-06-07 01:02:51 +0200

    Salut

--- Exercice3/run_mon_app.py
+++ Exercice3/run_mon_app.py
@@ -1,6 +1,5 @@
 # run_mon_app.py
-#
-# Juste un test
+# Bonjour
 # OM 2020.03.29 Démonstration de l'utilisation du microframework Flask
 # Des routes différentes sont définies
 # Le retour des données se fait grâce à une page en HTML et le langage JINJA
